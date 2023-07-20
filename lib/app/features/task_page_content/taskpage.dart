e7a854504338a77a0c6b2f98b5d42b6965f772ce
Damian Faron <damian.faron@onet.pl>
2023-07-20 13:39:47 +0200

    add CustomContainer

--- lib/app/features/task_page_content/taskpage.dart
+++ lib/app/features/task_page_content/taskpage.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class TaskPageContent extends StatelessWidget {
@@ -8,8 +7,43 @@ class TaskPageContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Lista tu będzie'),
+    return ListView(
+      children: const [
+        CustomContainer(),
+        CustomContainer(),
+      ],
+    );
+  }
+}
+
+class CustomContainer extends StatelessWidget {
+  const CustomContainer({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Container(
+        decoration: BoxDecoration(
+            gradient: const LinearGradient(colors: [
+              Color.fromARGB(80, 150, 87, 87),
+              Color.fromARGB(58, 218, 14, 14)
+            ], begin: Alignment.topCenter, end: Alignment.bottomRight),
+            color: Colors.blueAccent,
+            borderRadius: BorderRadius.circular(30)),
+        child: const Padding(
+          padding: EdgeInsets.all(12.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('tu zrobimy tytuł'),
+              Text('tu zrobimy opis pomysłu czy coś'),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }

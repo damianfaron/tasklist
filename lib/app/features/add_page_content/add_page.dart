2e114567c3d4edbdf2d3a566badf99348d3f5596
Damian Faron <damian.faron@onet.pl>
2023-07-20 14:48:28 +0200

    create add page

--- lib/app/features/add_page_content/add_page.dart
+++ lib/app/features/add_page_content/add_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class AddPageContent extends StatelessWidget {
@@ -8,8 +7,44 @@ class AddPageContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Dodaj zadanie do listy'),
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextField(
+            maxLines: null,
+            decoration: InputDecoration(
+              labelText: 'Nazwa zadania',
+              enabledBorder: OutlineInputBorder(
+                borderSide: const BorderSide(
+                  width: 3,
+                  color: Color.fromARGB(80, 150, 87, 87),
+                ),
+                borderRadius: BorderRadius.circular(30),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          TextField(
+            maxLines: null,
+            decoration: InputDecoration(
+              labelText: 'Opis zadania',
+              enabledBorder: OutlineInputBorder(
+                borderSide: const BorderSide(
+                  width: 3,
+                  color: Color.fromARGB(80, 150, 87, 87),
+                ),
+                borderRadius: BorderRadius.circular(30),
+              ),
+            ),
+          ),
+          TextField(),
+          Text('Dodaj zadanie do listy'),
+        ],
+      ),
     );
   }
 }
